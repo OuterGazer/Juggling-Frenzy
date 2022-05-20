e2d2ffbf2aed246c03f37560d553039e0d2366ac
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2022-05-20 23:11:57 +0200

    Implemented changing upward force depending on the height of the screen that the ball is being clicked.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,14 +8,28 @@ public class Ball : MonoBehaviour
     [SerializeField] float impulseForceAmount = default;
 
     private Rigidbody2D ballRB;
+    private int screenHeight;
 
     private void Awake()
     {
         this.ballRB = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        this.screenHeight = Screen.height;
+
+        this.ballRB.AddForce(Vector2.up * this.impulseForceAmount, ForceMode2D.Impulse);
+    }
+
     void OnMouseDown()
     {
-        this.ballRB.AddForce(Vector2.up * this.impulseForceAmount, ForceMode2D.Impulse);
+        // If we click the ball low on the screen it applies full force upward. The higher we click, the less force is applied.
+        // This is to have the upper deadpoint be always at roughly the same height to avoid ball flying off the screen.
+        float forceFactor = (1 - (Input.mousePosition.y / this.screenHeight)) * this.impulseForceAmount;
+
+        // We eliminate all velocity to manipulate the ball better and then apply the upwards force.
+        this.ballRB.velocity = Vector2.zero;
+        this.ballRB.AddForce(Vector2.up * forceFactor, ForceMode2D.Impulse);
     }
 }
