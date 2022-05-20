d618bd7a95a032ac3e493f8c51d0d85bdbb3f328
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2022-05-20 22:44:05 +0200

    Started implementing upwards impulse functionality.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float impulseForceAmount = default;
+
+    private Rigidbody2D ballRB;
+
+    private void Awake()
     {
-        
+        this.ballRB = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        this.ballRB.AddForce(Vector2.up * this.impulseForceAmount, ForceMode2D.Impulse);
     }
 }
