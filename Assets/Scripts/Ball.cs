3347663015335821e98517c3128725a0875659ee
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2022-05-21 10:47:50 +0200

    Added headers and tooltips to Ball script. Implemented incremental vertical drag to smoothen the balls velocity up and down while allowing a naturall sideways movement that the built in drag in Rigidbody2D prevents.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,22 +5,67 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Ball : MonoBehaviour
 {
+    [Header("Physics characteristics")]
+    [Tooltip("This is the force applied upwards upon ball click.")]
     [SerializeField] float impulseForceAmount = default;
+    [Tooltip("This is the childed reference point used to calculate the sideways moving. The closer it is to the ball, the more the ball will move towards the sides upon click.")]
     [SerializeField] Transform referencePoint;
+    [Tooltip("Normal drag applies in X and Y, this drag is used to exclusively dampen movement in Y so the ball moves naturally to the sides.")]
+    [SerializeField] float verticalDrag = default;
+    [Tooltip("This is the speed in which upwards drag is increased as the ball falls to dampen it's downwards velocity")]
+    [SerializeField] float dragApplicationSpeedFactor = default;
 
     private Rigidbody2D ballRB;
     private int screenHeight;
+    private float maxVertDrag;
+
+    private bool isBallFalling = false;
 
     private void Awake()
     {
         this.ballRB = this.gameObject.GetComponent<Rigidbody2D>();
+
     }
 
     private void Start()
     {
         this.screenHeight = Screen.height;
+        this.maxVertDrag = this.verticalDrag;
 
         this.ballRB.AddForce(Vector2.up * this.impulseForceAmount, ForceMode2D.Impulse);
+    }
+
+    private void Update()
+    {
+        ChangeDragOnBallVelocityDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        // Regular drag applies in all directions and prevents lateral moving of the ball
+        // Here I will apply a constant vertical drag contrary to the ball's current velocity direction
+        this.ballRB.AddForce(-Vector2.up * Mathf.Sign(this.ballRB.velocity.y) * this.verticalDrag, ForceMode2D.Force);
+    }
+
+    private void ChangeDragOnBallVelocityDirection()
+    {
+        if (this.ballRB.velocity.y <= 0 && !this.isBallFalling)
+        {
+            this.verticalDrag = 0.0f;
+            this.isBallFalling = true;
+        }
+        else if (this.ballRB.velocity.y < 0)
+        {
+            if (this.verticalDrag < this.maxVertDrag)
+            {
+                this.verticalDrag += Time.deltaTime * this.dragApplicationSpeedFactor;
+            }
+        }
+        else if (this.ballRB.velocity.y >= 0 && this.isBallFalling)
+        {
+            this.verticalDrag = this.maxVertDrag;
+            this.isBallFalling = false;
+        }
     }
 
     void OnMouseDown()
