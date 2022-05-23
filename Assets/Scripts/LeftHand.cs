5a06b7cffa2897285a27d003ba3b5bdbc554d0f4
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2022-05-23 10:15:05 +0200

    Implemented platform movement with new input system.

--- Assets/Scripts/LeftHand.cs
+++ Assets/Scripts/LeftHand.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LeftHand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement Characteristics")]
+    [Tooltip("Sets the lateral speed of the hand")]
+    [Range(0.0f, 10.0f)][SerializeField] float moveSpeed;
+
+
+    private Rigidbody2D handRB;
+    private Vector2 handMovement;
+
+
+    private void Awake()
     {
-        
+        this.handRB = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnMove(InputValue handMovement)
     {
-        
+        this.handMovement = new Vector2(handMovement.Get<Vector2>().x, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        this.handRB.MovePosition(this.handRB.position += (this.handMovement * this.moveSpeed * Time.fixedDeltaTime));
     }
 }
